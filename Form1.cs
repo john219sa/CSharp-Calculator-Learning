a10bb41310171352f0bb8c7e68cd4c0dbd475353
john safwat <john219saf@gmail.com>
2025-12-30 04:03:37 +0200

    Add try-catch to handle invalid user input
    Added error handling for number conversion and division by zero.

--- Form1.cs
+++ Form1.cs
@@ -9,37 +9,89 @@ namespace mathematicalOperation
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int num1 = Convert.ToInt32(txtNum1.Text);
-            int num2 = Convert.ToInt32(txtNum2.Text);
-            txtResult.Text = (num1 + num2).ToString();
+            try{
+                int num1 = Convert.ToInt32(txtNum1.Text);
+                int num2 = Convert.ToInt32(txtNum2.Text);
+                txtResult.Text = (num1 + num2).ToString();
+            }catch(Exception){
+            MessageBox.Show(
+                  "Please enter valid numbers only.",
+                    "Input Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+            );
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            int num1 = Convert.ToInt32(txtNum1.Text);
-            int num2 = Convert.ToInt32(txtNum2.Text);
-            txtResult.Text = (num1 - num2).ToString();
+            try{
+                int num1 = Convert.ToInt32(txtNum1.Text);
+                int num2 = Convert.ToInt32(txtNum2.Text);
+                txtResult.Text = (num1 - num2).ToString();
+            }catch(Exception){
+            MessageBox.Show(
+                  "Please enter valid numbers only.",
+                    "Input Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+            );
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            int num1 = Convert.ToInt32(txtNum1.Text);
-            int num2 = Convert.ToInt32(txtNum2.Text);
-            txtResult.Text = (num1 * num2).ToString();
+            try{
+                int num1 = Convert.ToInt32(txtNum1.Text);
+                int num2 = Convert.ToInt32(txtNum2.Text);
+                txtResult.Text = (num1 * num2).ToString();
+            }catch(Exception){
+            MessageBox.Show(
+                  "Please enter valid numbers only.",
+                    "Input Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+            );
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            int num1 = Convert.ToInt32(txtNum1.Text);
-            int num2 = Convert.ToInt32(txtNum2.Text);
-            txtResult.Text = (num1 / num2).ToString();
+            try{
+                int num1 = Convert.ToInt32(txtNum1.Text);
+                int num2 = Convert.ToInt32(txtNum2.Text);
+                if (num2 == 0)
+                        {
+                            MessageBox.Show("Cannot divide by zero.");
+                            return;
+                        }
+    
+                txtResult.Text = (num1 / num2).ToString();
+            }catch(Exception){
+            MessageBox.Show(
+                  "Please enter valid numbers only.",
+                    "Input Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+            );
+            }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            int num1 = Convert.ToInt32(txtNum1.Text);
-            int num2 = Convert.ToInt32(txtNum2.Text);
-            txtResult.Text = (num1 % num2).ToString();
+        try{
+                int num1 = Convert.ToInt32(txtNum1.Text);
+                int num2 = Convert.ToInt32(txtNum2.Text);
+                txtResult.Text = (num1 % num2).ToString();
+            }catch(Exception){
+            MessageBox.Show(
+                  "Please enter valid numbers only.",
+                    "Input Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+            );
+            }
         }
     }
 }
+
